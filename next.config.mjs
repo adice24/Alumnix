ce33b029231ff14aad3e2a77c93bf22ec0b4da01
Shilpashree <shilpak2k23@gmail.com>
2026-04-11 14:27:04 +0530

    chore: solve lighthouse performance penalties, JS bundle optimizations, and text compression errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compress: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion', 'gsap'],
+  },
+};
 
 export default nextConfig;
